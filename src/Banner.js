ce62a1a60425a4a4a91707f5f0af199e6d575db2
CcharlotteHh <210547@student.glu.nl>
2023-01-16 21:29:00 +0100

    progress

--- src/Banner.js
+++ src/Banner.js
@@ -1,24 +1,26 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "./axios";
-import requests from './requests';
+import requests from "./requests";
 
 function Banner() {
-const [movie , setMovie] = useState([]);    
-useEffect(() => {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
     async function fetchData() {
       const request = await axios.get(requests.fetchNetflixOriginals);
-      Math.floor(Math.random()* request.data.lenght -1)
-      setMovie(request.data.results);
-      return request;
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ]
+      );
+      return request
     }
     fetchData();
   }, []);
-   
-  return (
-    <header>
-      
-    </header>
-  )
+
+  console.log(movie)
+
+  return <header></header>;
 }
 
-export default Banner
+export default Banner;
